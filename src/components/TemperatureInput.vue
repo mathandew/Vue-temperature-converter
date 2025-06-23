d7dbecbaf22bb2d552508794750a57baab4888df
Mathan Dewpal <mathandewpal514@gmail.com>
2025-06-23 17:47:54 +0500

    fix: made some changes in view

---
+++ src/components/TemperatureInput.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="input-box">
+    <label for="input">Enter Temperature:</label>
+    <input
+      id="input"
+      type="number"
+      :value="modelValue"
+      @input="$emit('update:modelValue', parseFloat($event.target.value))"
+      placeholder="Enter value"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: Number
+  }
+};
+</script>
+
+<style scoped>
+.input-box {
+  margin: 20px 0;
+}
+input {
+  padding: 10px;
+  width: 100%;
+  font-size: 16px;
+}
+</style>
